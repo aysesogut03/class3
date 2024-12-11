054249a8b96c6a77b286f176fbe7af0ecc3357bb
aysesogut03 <aysesogut2003@gmail.com>
2024-12-11 09:46:09 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bilgisayar bilgisayar1=new bilgisayar("siyah",2017,"Dell");
+
+            Console.WriteLine("Bilgisayar rengi:" + bilgisayar1.pcRengi);
+            Console.WriteLine("Bilgisayar üretim yılı:"+bilgisayar1.modelYili);
+            Console.WriteLine("Bilgisayar model:"+bilgisayar1.modelYili);
+            bilgisayar1.calistir();
+            bilgisayar1.sifregir();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bilgisayar.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{
+    internal class bilgisayar
+    {
+        public string pcRengi;
+        public int modelYili;
+        public string marka;
+
+        public bilgisayar( string _pcRengi, int _modelYili,string _marka)
+        {
+            pcRengi= _pcRengi;
+            modelYili= _modelYili;  
+            marka= _marka; 
+        }
+        public void calistir()
+        {
+            Console.WriteLine("Bilgisayar çalıştırılıyor...");
+        }
+        public void sifregir()
+        {
+            Console.WriteLine("Şifre giriliyor...");
+        }
+    }
+}
